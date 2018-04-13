73d6aaddf87e3dc7417a09e51c684df50b2c5c80
batyr <batyr71@gmail.com>
2018-04-13 20:03:15 +0300

    1page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './scss/home.scss';
+import './images/logo.png';
+import './images/call-answer.svg';
+import './images/call-answer.png';
+import './images/trash.png';
+import './images/mail-envelope.png';
+import './images/placeholder.png';
+import './images/dropdown-icon.png';
