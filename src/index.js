d3ce50582e0f4a3b118e6aa2c86271a142d2b489
Batyr <batyr71@gmail.com>
2018-04-14 23:27:59 +0300

    accordeon works

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ import './images/trash.png';
 import './images/mail-envelope.png';
 import './images/placeholder.png';
 import './images/dropdown-icon.png';
+
+
+  document.querySelector(".open-toggler").addEventListener('click', function () {
+    let par = this.parentElement;
+    if(par.style.maxHeight){
+      par.style.maxHeight = null;
+    } else {
+      par.style.maxHeight = "100%"
+      par.style.overflow = "hidden"
+    }
+  });
